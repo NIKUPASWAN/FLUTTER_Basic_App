8dce50636110a7985c6b087c25af5bec63c36569
NIKUPASWAN <alwayswindream610@gmail.com>
2020-06-15 10:34:25 +0530

    Ch3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,32 +14,47 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('AueSome App'),
       ),
-      body: Center(child: Container(
-        padding: EdgeInsets.all(10),
-        alignment: Alignment.center,
-       
-        height: 200,
-        width: 200,
-        child: ( Text('Hi flutter ',
-        style: TextStyle(fontWeight: FontWeight.bold,
-        fontSize: 30,color: Colors.white),))
-        ,decoration: BoxDecoration(
-         // shape: BoxShape.circle,
-         borderRadius: BorderRadius.circular(20),
-         boxShadow:[
-           BoxShadow(color: Colors.yellow[100],
-           blurRadius: 10,
-           offset: Offset(10,12)
-           
-           
-            ) ] ,
-          color: Colors.teal,
-          gradient:LinearGradient (colors:[
-         Colors.orange,Colors.yellow   
-          ] ,
-          )
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+              child: Center(child: 
+        Column(mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        
+          children: <Widget>[
+          
+          Container (
+            padding: EdgeInsets.all(10),
+            height: 100,
+            width: 100,
+            alignment: Alignment.center,
+            color: Colors.yellow,
+            child: Text('hi flutter',
+            )
+            ,),
+            Container (
+            padding: EdgeInsets.all(10),
+            height: 100,
+            width: 100,
+            alignment: Alignment.center,
+            color: Colors.red,
+            child: Text('red',
+            )
+            ,),
+            Container (
+            padding: EdgeInsets.all(10),
+            height: 100,
+            width: 100,
+            alignment: Alignment.center,
+            color: Colors.green,
+            child: Text('hi flutter',
+            )
+            ,),
+        ]
+        
+        ,)
         ),
-      ) ,)
+      )
     );
   }
 }
