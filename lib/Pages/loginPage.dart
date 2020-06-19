76596da077c245442ea7673e3d4c6d971e0dbe27
NIKUPASWAN <alwayswindream610@gmail.com>
2020-06-19 14:42:10 +0530

    ch5com

---
+++ lib/Pages/loginPage.dart
@@ -0,0 +1,76 @@
+
+//import 'package:APK/Pages/homePages.dart';
+import 'package:APK/bgImage.dart';
+import 'package:flutter/material.dart';
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final userNameController=TextEditingController();
+  final paswordNameController=TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text('Login pages'),
+        
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children :<Widget>[
+BgImage(),
+Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child:Center(
+            child: Card(child: 
+            Column(children: <Widget>[
+ Form(child:Padding(
+   padding: const EdgeInsets.all(8.0),
+   child: Column(children: <Widget>[
+   
+     TextFormField(
+   
+       decoration:InputDecoration(
+   
+      hintText:'Enter name',labelText:'Name' ),
+   
+     ),
+   
+     SizedBox(height: 16,),
+   
+     TextFormField(
+   obscureText: true,
+       decoration:InputDecoration(
+   
+      hintText:'Enter pasword',labelText:'pasword' ),
+   
+     ),
+   
+   ]
+   
+   
+   
+   ),
+ ),
+ ),
+ SizedBox(height: 16,),
+ RaisedButton(onPressed: () {
+   Navigator.pushNamed(context, "/home");
+ },
+ child: Text('sign In'),
+ color: Colors.orange,
+ textColor: Colors.white,
+ )
+              ]
+              ,),
+            ),
+          )
+        ),
+      ),
+      ],)
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
+
+
 import 'package:APK/Pages/homePages.dart';
+import 'package:APK/Pages/loginPage.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
     title: 'AuweSome App',
-    home:HomePage(),
+    home:LoginPage(),
     theme: ThemeData(
       primarySwatch:Colors.purple
     ),
+    routes: {
+      "/Longin":(context)=>LoginPage(),
+      "/home":(context)=>HomePage()
+    },
   ),);
 }
 
